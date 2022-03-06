5eee0246c60836111a62792c9bc15e7055c97719
rameem2003 <rameem2019@gmail.com>
2022-03-06 21:18:44 +0600

    Add project files.

---
+++ single_inheritance/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace single_inheritance
+{
+    class salary
+    {
+        public int member_salary = 2000;
+    }
+
+    class bonus: salary
+    {
+        public int member_bonus = 5000;
+    }
+
+
+    class app
+    {
+        public static void Main()
+        {
+            bonus obj = new bonus();
+
+            Console.WriteLine("Member salary is: " + obj.member_salary + " TK");
+            Console.WriteLine("Member bonus is: " + obj.member_bonus + " TK");
+        }
+    }
+}
